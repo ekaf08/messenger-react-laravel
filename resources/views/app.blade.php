0045aa9b04f6d03e3cf65071a6453857d4e503ea
ekaf08 <105408310+ekaf08@users.noreply.github.com>
2024-06-24 14:42:33 +0700

    rename folder resources/js

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -13,10 +14,12 @@
         <!-- Scripts -->
         @routes
         @viteReactRefresh
-        @vite(['resources/js/app.tsx', "resources/js/Pages/{$page['component']}.tsx"])
+        @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
         @inertiaHead
     </head>
+
     <body class="font-sans antialiased">
         @inertia
     </body>
+
 </html>
