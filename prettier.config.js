709de9e02665fe527b1626b4843d3dc5b7143b32
ekaf08 <105408310+ekaf08@users.noreply.github.com>
2024-06-24 14:41:57 +0700

    prettier, dan editor config

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+export default {
+    plugins: ["prettier-plugin-tailwindcss"],
+    tailwindFunctions: ["clsx"],
+};
+
+// prettier.config.js
+// module.exports = {
+//     plugins: [require('prettier-plugin-tailwindcss')],
+//     tailwindFunctions: ["clsx"],
+// };
