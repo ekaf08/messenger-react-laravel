a4c4aaceab60745f120097c4438caa356706116b
ekaf08 <105408310+ekaf08@users.noreply.github.com>
2024-07-01 11:22:57 +0700

    1.27 tampilan login

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,7 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'id' => Str::uuid(), // Generate a UUID
+            // 'id' => Str::uuid(), // Generate a UUID
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -4,15 +4,14 @@ import { PropsWithChildren } from 'react';
 
 export default function Guest({ children }: PropsWithChildren) {
     return (
-        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <Link href="/">
+        <div className="min-h-screen flex flex-col justify-center items-center ">
+            <div className='flex w-full max-w-md flex-col space-y-6 overflow-hidden rounded-lg bg-background p-6 shadow-md'>
+                <Link href="/" className='mx-auto w-20'>
                     <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
                 </Link>
-            </div>
 
-            <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {children}
+
             </div>
         </div>
     );
